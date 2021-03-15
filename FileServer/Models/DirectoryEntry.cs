15b585049435937e304eb6ad9f1cb717c3bef028
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-15 23:51:18 +0300

    Remove id, add path

--- FileServer/Models/DirectoryEntry.cs
+++ FileServer/Models/DirectoryEntry.cs
@@ -2,8 +2,8 @@
 {
     public sealed class DirectoryEntry
     {
-        public int Id { get; set; }
         public DirectoryEntryType Type { get; set; }
         public string Name { get; set; }
+        public string Path { get; set; }
     }
 }
--- FileServer/Models/DirectoryEntryType.cs
+++ FileServer/Models/DirectoryEntryType.cs
@@ -3,6 +3,6 @@
     public enum DirectoryEntryType
     {
         Directory,
-        File
+        File,
     }
 }
