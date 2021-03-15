6ee7e36cc8922d8c990b84d079bcf142c48e4159
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-15 23:51:31 +0300

    Add file service

---
+++ FileServer/Services/Interfaces/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using FileServer.Models;
+
+namespace FileServer.Services.Interfaces
+{
+    public interface IFileService
+    {
+        IEnumerable<DirectoryEntry> GetDirectoryEntries(string path);
+        DirectoryEntry CreateDirectory(string parentPath, string name);
+    }
+}
