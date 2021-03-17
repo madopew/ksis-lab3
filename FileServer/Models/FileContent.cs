c2ca8eb11c29bd056483fb471d779636da82046d
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-17 10:50:46 +0300

    Add model for file

---
+++ FileServer/Models/FileContent.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace FileServer.Models
+{
+    public sealed class FileContent
+    {
+        public Stream FileStream { get; set; }
+        public string ContentType { get; set; }
+    }
+}
