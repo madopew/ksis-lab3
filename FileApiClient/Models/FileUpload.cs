45ad39675728c26a82da77b4d78fdeb722bf2bbc
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-18 22:13:30 +0300

    Add json serialization

--- FileApiClient/Models/FileUpload.cs
+++ FileApiClient/Models/FileUpload.cs
@@ -1,9 +1,16 @@
-﻿namespace FileApiClient.Models
+﻿using Newtonsoft.Json;
+
+namespace FileApiClient.Models
 {
     public sealed class FileUpload
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
+        
+        [JsonProperty("contenttype")]
         public string ContentType { get; set; }
+        
+        [JsonProperty("filebase64")]
         public string FileBase64 { get; set; }
     }
 }
