0f1f9bbf12e684b131c808f69a239353d90d9398
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-18 18:20:14 +0300

    Update model

--- FileApiClient/Models/DirectoryEntry.cs
+++ FileApiClient/Models/DirectoryEntry.cs
@@ -1,9 +1,13 @@
-﻿namespace FileApiClient.Models
+﻿using System;
+
+namespace FileApiClient.Models
 {
     public sealed class DirectoryEntry
     {
         public DirectoryEntryType Type { get; set; }
         public string Name { get; set; }
         public string Path { get; set; }
+        public DateTime LastModified { get; set; }
+        public long Size { get; set; }
     }
 }
