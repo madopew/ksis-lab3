0ca7bcc80d31f7fb036a163eb0bb168b3894dfe6
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-18 17:33:05 +0300

    Fix ignore file

--- FileApiClient/Models/DirectoryEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileApiClient.Models
-{
-    public sealed class DirectoryEntry
-    {
-        public DirectoryEntryType Type { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
-    }
-}
--- FileServer/Models/DirectoryEntry.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FileServer.Models
-{
-    public sealed class DirectoryEntry
-    {
-        public DirectoryEntryType Type { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
-    }
-}
