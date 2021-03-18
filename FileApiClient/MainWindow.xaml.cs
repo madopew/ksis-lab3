b5e7b92243b7ac5a688c8de5b6dc50c9c03d128a
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-18 18:18:28 +0300

    Add prototype for gui

--- FileApiClient/MainWindow.xaml.cs
+++ FileApiClient/MainWindow.xaml.cs
@@ -1,13 +1,38 @@
-﻿namespace FileApiClient
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FileApiClient.Annotations;
+
+namespace FileApiClient
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public partial class MainWindow : INotifyPropertyChanged
     {
+        private string currentPath;
+        public string CurrentPath
+        {
+            get => currentPath;
+            private set
+            {
+                currentPath = value;
+                OnPropertyChanged(nameof(CurrentPath));
+            }
+        }
+        
         public MainWindow()
         {
+            CurrentPath = "/";
             InitializeComponent();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
