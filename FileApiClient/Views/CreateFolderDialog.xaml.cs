b26031bcc2469d60b8eda92edddc41beba9445de
Bakyt Madi <bakyt.madi.work@gmail.com>
2021-03-18 21:39:00 +0300

    Add create folder feature

---
+++ FileApiClient/Views/CreateFolderDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace FileApiClient.Views
+{
+    public partial class CreateFolderDialog : Window
+    {
+        private readonly Regex alphaRegex = new Regex("[^a-zA-Z]+");
+        
+        public string FolderName { get; set; }
+        public CreateFolderDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void FolderNameTextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = alphaRegex.IsMatch(e.Text);
+        }
+
+        private void CreateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(FolderNameTextBox.Text))
+            {
+                DialogResult = true;
+                FolderName = FolderNameTextBox.Text;
+            }
+
+            Close();
+        }
+
+        private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
